0530a44c71aa68b0644d24a78d83b6b873878163
Lucas Costa <englucascbmelo@gmail.com>
2024-10-01 11:19:47 -0300

    passando as novas propriedades para o construtor da classe Smartphone e corrigindo as classes Iphone e Nokia

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
 
-Iphone apple = new Iphone("(83) 99999-8888");
+Iphone apple = new Iphone("(83) 99999-8888", "iphone-16", "777777", 66666);
 
 apple.Ligar();
 apple.InstalarAplicativo("whatsapp");
