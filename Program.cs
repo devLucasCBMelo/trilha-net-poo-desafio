3c5474e989dd43e59b1cb2b59615dcb1ebfbd583
Lucas Costa <englucascbmelo@gmail.com>
2024-10-01 13:30:14 -0300

    subindo projeto

--- Program.cs
+++ Program.cs
@@ -6,3 +6,12 @@ Iphone apple = new Iphone("(83) 99999-8888", "iphone-16", "777777", 66666);
 
 apple.Ligar();
 apple.InstalarAplicativo("whatsapp");
+apple.ReceberLigacao();
+apple.Numero = "(83) 98888-5555";
+
+Console.WriteLine("\n");
+
+Nokia nokia = new Nokia("(82) 91111-2222", "indestrutivel-62", "87aska3", 551234);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
+nokia.ReceberLigacao();
